ad0a5b376de801fd9fe81e3e93944813425b1351
ctmazarura95 <mazaruracollins@gmail.com>
2021-04-25 17:05:34 +0200

    styling changes to banner

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -7,14 +7,16 @@ const MainPage = () => {
         <>
 
         <div class="wrapper ">
-            <div class="item">
-                <h1>Next generation digital <br/> banking </h1>
+            <div class="item ">
+               <div className="content">
+               <h1>Next generation digital <br/> banking </h1>
                 <p>Take your financial life online. Your Easybank <br/>
                  account will be a one-stop-shop for spending,<br/>
                   saving,budgeting, investing, and much more.</p>
+               </div>
             </div>
 
-            <div class="item">
+            <div class="item item-img">
                 <img id="banner-img" src={banner} alt=""/>
             </div>
         </div>
